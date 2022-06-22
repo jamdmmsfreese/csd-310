4477a6ba10817c73b91beb5ea4e4d3676d10ce2c
DESKTOP-0Q43626\Michele <mspeidel@my365.bellevue.edu>
2022-06-21 21:34:11 -0500

    deleted unneeded files

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -4,6 +4,6 @@ url = "mongodb+srv://admin:admin@cluster0.ptdopkt.mongodb.net/pytech?retryWrites
 ca = certifi.where()
 client = MongoClient(url,tlsCAFile=ca)
 db = client.pytech
-print("-- Pytech Collection List")
+print("-- Pytech Collection List --")
 print(db.list_collection_names())
 input("\n\n  End of program, press any key to exit... ")
