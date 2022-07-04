ae1ac5655ed92d3b69a75e97f6bafc64fcedb3d4
DESKTOP-0Q43626\Michele <mspeidel@my365.bellevue.edu>
2022-07-03 22:15:35 -0500

    module_8

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -27,29 +27,3 @@ kim = {"student_id": 1009,
 kim_student_id = insert_one(kim)
 print("Inserted student record {} {} into the students collection with document_id {}".format(kim["first_name"], kim["last_name"], kim_student_id))
 input("\n\n  End of program, press any key to exit... ")
-
-
-===
-print ("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
-docs = students.find({})
-for doc in docs:
-    print("\nStudent ID: ", doc["student_id"], "\nFirst Name: ", doc["first_name"], "\nLast Name: ", doc["last_name"])
-    print()
-
-def insert_one(student): 
-    return students.insert_one(student).inserted_id
-print ("-- INSERT STATEMENTS --")
-kenny = {"student_id": 1010,
-    "first_name": "Kenny",
-    "last_name": "Smith"}
-kenny_student_id = insert_one(kenny)
-print("Inserted student record {} {} into the students collection with document_id {}".format(kenny["first_name"], kenny["last_name"], kenny_student_id))
-print()
-
-print ("-- DISPLAYING NEW STUDENT LIST DOC --")
-docs = students.find({})
-for doc in docs:
-    print("\nStudent ID: ", doc["student_id"], "\nFirst Name: ", doc["first_name"], "\nLast Name: ", doc["last_name"])
-    print()
-    
-doc.students.remove_one({"student_id": "1010"})
